76daedaa899d0cef9796a1c7dc76d200e845c615
dayoungpyo <144453750+celestedayoung@users.noreply.github.com>
2024-01-22 23:42:14 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 204 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] letters = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String testCase = sc.next();
+
+        int cnt = 0;
+        int left = testCase.length();
+
+        for (int l = 0; l < letters.length; l++) {
+            int pointer = 0;
+
+            for (int t = 0; t <= testCase.length() - letters[l].length(); t++) {
+                String compare = testCase.substring(pointer, pointer + letters[l].length());
+
+                if (letters[l].equals(compare)) {
+                    left -= letters[l].length();
+//                    String remove = testCase.substring(0, pointer + letters[l].length());
+                    testCase = testCase.replaceFirst(compare, "x");
+                    cnt++;
+                }
+                pointer++;
+            }
+        }
+        System.out.println(cnt + left);
+    }
+}
