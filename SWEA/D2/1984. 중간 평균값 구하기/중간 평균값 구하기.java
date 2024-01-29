b99f73c51b3683ef5e1bebc0b30c37b42ed5205f
dayoungpyo <144453750+celestedayoung@users.noreply.github.com>
2024-01-30 08:11:25 +0900

    [D2] Title: 중간 평균값 구하기, Time: 137 ms, Memory: 21,632 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -1,33 +1,42 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-        for (int t = 1; t <= T; t++) {
-
-            List<Integer> nums = new ArrayList<>();
-
-            for (int i = 0; i < 10; i++) {
-                nums.add(sc.nextInt());
-            }
-
-            nums.sort(null);
-            nums.remove(0);
-            nums.remove(8);
-
-            int sum = 0;
-            for (int i = 0; i < nums.size(); i++) {
-                sum += nums.get(i);
-            }
-
-            double avg = (double) sum / nums.size();
-            System.out.printf("#%d %.0f%n", t, avg);
-        }
-    }
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int t = 1; t <= T; t++) {
+			
+			int[] nums = new int[10];
+            
+           for (int n = 0; n < 10; n++) {
+				nums[n] = sc.nextInt();
+			}
+			
+			for (int i = 0; i < nums.length - 1; i++) {
+				for (int j = 0; j < nums.length - 1 - i; j++) {
+					
+					if (nums[j] > nums[j + 1]) {
+						
+						int tmp = nums[j];
+						nums[j] = nums[j + 1];
+						nums[j + 1] = tmp;
+					}
+					
+				}
+			}
+			
+			int sum = 0;
+			for (int s = 1; s < nums.length - 1; s++) {
+				sum += nums[s];
+			}
+			
+			double avg = (double) sum / (nums.length - 2);
+			
+			System.out.printf("#%d %.0f%n", t, avg);
+		}
+	}
 }
