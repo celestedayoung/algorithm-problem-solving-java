ba3f9360d79862e39c1c5c3e721d24064cf00454
dayoungpyo <144453750+celestedayoung@users.noreply.github.com>
2024-03-03 21:08:59 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 189 ms, Memory: 31,432 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.java
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.java
@@ -1,143 +1,83 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
+    static Scanner sc;
+    static int testNum;
+    static String input;
+    static char[][] board;
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
 
-        for (int t = 1; t <= 10; t++) {
-            int testNum = sc.nextInt();
+        for (int t = 1; t <= 10 ; t++) {
 
-            char[][] board = new char[100][];
+            testNum = sc.nextInt();
+            sc.nextLine();
+
+            board = new char[100][100];
 
             for (int i = 0; i < 100; i++) {
-                String stringRow = sc.next();
-                board[i] = stringRow.toCharArray();
+                input = sc.nextLine();
+                for (int j = 0; j < 100; j++) {
+                    board[i][j] = input.charAt(j);
+                }
             }
 
-            int rowMax = rowPalindrome(board);
-            int colMax = colPalindrome(board);
+            int max = -1;
 
-            int result = (rowMax <= colMax) ? colMax : rowMax;
+            // row direction
+            // palindromes' length % 2 == 0
+            // palindromes' length % 2 != 0
+            for (int r = 0; r < 100; r++) {
+                for (int c = 0; c < 100; c++) {
 
-            System.out.printf("#%d %d%n", testNum, result);
+                    int pointer1 = 1;
+                    while (c - pointer1 >= 0 && c + pointer1 < 100 && board[r][c - pointer1] == board[r][c + pointer1]) {
+                        int length = 1 + 2 * pointer1;
+                        if (length > max) {
+                            max = length;
+                        }
+                        pointer1++;
+                    }
 
+                    int pointer2 = 0;
+                    while (c - 1 - pointer2 >= 0 && c + pointer2 < 100 && board[r][c - 1 - pointer2] == board[r][c + pointer2]) {
+                        int length = (pointer2 + 1) * 2;
+                        if (length > max) {
+                            max = length;
+                        }
+                        pointer2++;
+                    }
+                }
+            }
+
+            // col direction
+            // palindromes' length % 2 == 0
+            // palindromes' length % 2 != 0
+            for (int r = 0; r < 100; r++) {
+                for (int c = 0; c < 100; c++) {
+
+                    int pointer1 = 1;
+                    while (r - pointer1 >= 0 && r + pointer1 < 100 && board[r - pointer1][c] == board[r + pointer1][c]) {
+                        int length = 1 + 2 * pointer1;
+                        if (length > max) {
+                            max = length;
+                        }
+                        pointer1++;
+                    }
+
+                    int pointer2 = 0;
+                    while (r - 1 - pointer2 >= 0 && r + pointer2 < 100 && board[r - 1 - pointer2][c] == board[r + pointer2][c]) {
+                        int length = (pointer2 + 1) * 2;
+                        if (length > max) {
+                            max = length;
+                        }
+                        pointer2++;
+                    }
+                }
+            }
+            System.out.printf("#%d %d%n", testNum, max);
         }
-    }
-
-    public static int colPalindrome(char[][] charArr) {
-
-        int max = Integer.MIN_VALUE;
-
-        // palindrome의 길이가 홀수인 경우
-        for (int c = 0; c < charArr.length; c++) {
-            for (int i = 0; i < charArr[c].length; i++) {
-
-                int idx = 0;
-
-                while (true) {
-                    idx++;
-                    if (i - idx >= 0 && i + idx < charArr[c].length) {
-                        if (charArr[i - idx][c] == charArr[i + idx][c]) {
-
-                            int length = 2 * idx + 1;
-
-                            if (max <= length) {
-                                max = length;
-                            }
-                        } else {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            }
-
-            // palindrome의 길이가 짝수인 경우
-            for (int i = 0; i < charArr[c].length; i++) {
-
-                int idx = -1;
-
-                while (true) {
-                    idx++;
-                    if (i - idx - 1 >= 0 && i + idx < charArr[c].length) {
-                        if (charArr[i - idx - 1][c] == charArr[i + idx][c]) {
-
-                            int length = 2 * (idx+1);
-
-                            if (max <= length) {
-                                max = length;
-                            }
-                        } else {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-        return max;
-
-    }
-
-    public static int rowPalindrome(char[][] charArr) {
-
-        int max = Integer.MIN_VALUE;
-
-        for (int r = 0; r < charArr.length; r++) {
-
-            // palindrome의 길이가 홀수인 경우
-            for (int i = 0; i < charArr[r].length; i++) {
-
-                int idx = 0;
-
-                while (true) {
-                    idx++;
-
-                    if (i - idx >= 0 && i + idx < charArr[r].length) {
-                        if (charArr[r][i - idx] == charArr[r][i + idx]) {
-
-                            int length = 2 * idx + 1;
-
-                            if (max <= length) {
-                                max = length;
-                            }
-                        } else {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            }
-
-            // palindrome의 길이가 짝수인 경우
-            for (int i = 0; i < charArr[r].length; i++) {
-
-                int idx = -1;
-
-                while (true) {
-                    idx++;
-                    if (i - idx - 1 >= 0 && i + idx < charArr[r].length) {
-                        if (charArr[r][i - idx - 1] == charArr[r][i + idx]) {
-
-                            int length = 2 * (idx+1);
-
-                            if (max <= length) {
-                                max = length;
-                            }
-                        } else {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-        return max;
     }
 }
