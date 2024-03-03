a5ec5a8f355dc7a0c92e59d0d42c8b9671adc648
dayoungpyo <144453750+celestedayoung@users.noreply.github.com>
2024-03-03 19:51:38 +0900

    [D2] Title: 파리 퇴치, Time: 140 ms, Memory: 20,816 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,56 +1,52 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int testNum = sc.nextInt();
-		
-		for (int t = 1; t <= testNum; t++) {
-			
-			int N = sc.nextInt();
-			int killRange = sc.nextInt();
-			
-			int[][] fly = new int[N][N];
-			
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					fly[i][j] = sc.nextInt();
-				}	
-			}
-			
-			List<Integer> sumList = new ArrayList<Integer>();
-			
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < N; c++) {
-					
-					int sum = 0;
-					
-					for (int nr = 0; nr < killRange; nr++) {
-						for (int nc = 0; nc < killRange; nc++) {
-							if (r + nr < N && c + nc < N) {
-								sum += fly[r + nr][c + nc];
-							}
-						
-							
-						}
-					}
-					sumList.add(sum);
-				}
-			}
-			
-			int max = -1;
-			
-			for (int i = 0; i < sumList.size(); i++) {
-				if (max < sumList.get(i)) {
-					max = sumList.get(i);
-				}
-			}
-			System.out.printf("#%d %d%n", t, max);
-		}
-	}
+    static Scanner sc;
+    static StringTokenizer st;
+    static int testNum;
+    static int N;
+    static int M;
+    static int[][] flies;
+
+    public static void main(String[] args) {
+
+        sc = new Scanner(System.in);
+        testNum = sc.nextInt();
+
+        for (int t = 1; t <= testNum ; t++) {
+
+            N = sc.nextInt();
+            M = sc.nextInt();
+            sc.nextLine();
+
+            flies = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(sc.nextLine());
+                for (int j = 0; j < N; j++) {
+                    flies[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int max = 0;
+            for (int r = 0; r <= N - M; r++) {
+                for (int c = 0; c <= N - M; c++) {
+
+                    int kill = 0;
+
+                    for (int i = r; i < r + M; i++) {
+                        for (int j = c; j < c + M; j++) {
+                            kill += flies[i][j];
+                        }
+                    }
+                    if (kill > max) {
+                        max = kill;
+                    }
+                }
+            }
+            System.out.printf("#%d %d%n", t, max);
+
+        }
+    }
 }
