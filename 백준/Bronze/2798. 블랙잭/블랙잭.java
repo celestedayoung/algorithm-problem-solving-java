c1ed80b196a4ff3ee3fbe2aebe232ffb3d00dd74
dayoungpyo <144453750+celestedayoung@users.noreply.github.com>
2024-09-11 21:35:21 +0900

    [Bronze II] Title: 블랙잭, Time: 164 ms, Memory: 23496 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,37 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc  = new Scanner(System.in);
-        
-        int cardNum = sc.nextInt();
-        int sumNum = sc.nextInt();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, M;
+    static int[] cardSet;
 
-        int[] cards = new int[cardNum];
-        for (int i = 0; i < cardNum; i++) {
-            cards[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        cardSet = new int[N];
+        for (int i = 0; i < cardSet.length; i++) {
+            cardSet[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<Integer> cases = new ArrayList<>();
-        for (int i = 0; i < cardNum - 2; i++) {
-            for (int j = i + 1; j < cardNum - 1; j++) {
-                for (int k = j + 1; k < cardNum; k++) {
-                    cases.add(cards[i] + cards[j] + cards[k]);
+        List<Integer> diffList = new ArrayList<>();
+        List<Integer> cardSetList = new ArrayList<>();
+        for (int i = 0; i < cardSet.length - 2; i++) {
+            int sum = 0;
+            for (int j = i + 1; j < cardSet.length - 1; j++) {
+                for (int k = j + 1; k < cardSet.length; k++) {
+                    sum = cardSet[i] + cardSet[j] + cardSet[k];
+                    if ( M - sum >= 0) {
+                        diffList.add(M - sum);
+                        cardSetList.add(sum);
+                    }
+
                 }
             }
         }
-        int min = Integer.MAX_VALUE;
-        int result = 0;
-        for (int i = 0; i < cases.size(); i++) {
-            if (min > Math.abs(sumNum - cases.get(i)) && cases.get(i) <= sumNum) {
-                min = Math.abs(sumNum - cases.get(i));
-                result = cases.get(i);
-            }
-        }
 
-        System.out.println(result);
+        int minIndex = diffList.indexOf(Collections.min(diffList));
+        System.out.println(cardSetList.get(minIndex));
+
+
+
+
+
     }
+
 }
